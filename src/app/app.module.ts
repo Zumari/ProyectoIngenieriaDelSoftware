4f710b0443acc81105f4d53813b4fda033e3a313
Zumari <fernanda_zunigaa@yahoo.com>
2021-11-01 15:17:03 -0600

    estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { UserComponent } from './components/user/user.component';
+import { DashboardComponent } from './components/user/dashboard/dashboard.component';
+import { MyeventsComponent } from './components/user/myevents/myevents.component';
+import { StatisticsComponent } from './components/user/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    UserComponent,
+    DashboardComponent,
+    MyeventsComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
