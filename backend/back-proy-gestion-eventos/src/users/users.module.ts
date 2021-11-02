3c958c83989e7713655b7549ce89514feac4de20
KevinVarela504 <83558063+KevinVarela504@users.noreply.github.com>
2021-11-02 14:25:38 -0600

    Users Module

--- backend/back-proy-gestion-eventos/src/users/users.module.ts
+++ backend/back-proy-gestion-eventos/src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Users_ } from './Entities/user.entity';
+import { Users} from './Entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Users_])],
+    imports:[TypeOrmModule.forFeature([Users])],
     controllers:[UsersController],
     providers:[UsersService],
     exports:[TypeOrmModule,UsersService]
