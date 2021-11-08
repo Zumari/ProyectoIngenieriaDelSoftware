318627f708baae8c91330bf341b1dae67edaa9a8
eddyone7 <93343079+eddyone7@users.noreply.github.com>
2021-11-08 08:39:30 -0600

    Update events.service.ts

--- src/app/services/user/events/events.service.ts
+++ src/app/services/user/events/events.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Event } from 'src/app/interfaces/event';
+import { Status } from 'src/app/interfaces/status';
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  createEvent(event:Event):Observable<Event>{
+    return this.httpClient.post<Event>('http://localhost:3000/events/createEvent',{});
+   }
+
+  /*getEventsForUser(email : string):Observable<Event[]>{
+   return this.httpClient.get<Event[]>(`http://localhost:3000/events/${email}/`,{});
+  }*/
+
+  getAllEvents():Observable<Event[]>{
+    return this.httpClient.get<Event[]>(`http://localhost:3000/events/getAllEvents`,{});
+   }
+ 
+  
+  getEvent(idEvento:number):Observable<Event>{
+    return this.httpClient.get<Event>(`http://localhost:3000/events/getOneEventById/${idEvento}`,{});
+   }
+
+  updateEvent(idEvento:number, event:Event):Observable<Event>{
+    return this.httpClient.put<Event>(`http://localhost:3000/events/updateEvent/${idEvento}`,{});
+   } 
+
+  deleteEvent(idEvento:number):Observable<Event>{
+   return this.httpClient.delete<Event>(`http://localhost:3000/events/deleteEvent/${idEvento}`,{});
+  }
+
+
+
+//Status 
+
+
+  getStatus(statusId: number):Observable<Status>{
+   return this.httpClient.get<Status>(`http://localhost:/status/getStatus/${statusId}`,{});
+  }
+
+  
+  getAllStatus():Observable<Status[]>{
+    return this.httpClient.get<Status[]>(`http://localhost:/status/getAll`,{});
+   }
+
+  updateStatus(statusId:number):Observable<Status>{
+    return this.httpClient.put<Status>(`http://localhost:/status/${statusId}/`,{});
+   } 
+
+
+
+
+
 }
