176320bc7ec2c3b593e157150da9706790a5291f
Jiordiro <ouchihajd11@gmail.com>
2021-11-23 16:54:40 -0600

    EventImages BACK-BD

--- backend/back-proy-gestion-eventos/src/event-images/event-images.module.ts
+++ backend/back-proy-gestion-eventos/src/event-images/event-images.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventImages } from './entities/event-images.entity';
+import { EventImagesController } from './event-images.controller';
 import { EventImagesService } from './event-images.service';
 
 @Module({
-  providers: [EventImagesService]
+
+  imports:[
+      TypeOrmModule.forFeature([EventImages])  ]
+      ,
+      controllers:[
+          EventImagesController],
+      providers:[
+          EventImagesService
+      ],
+      exports:[
+          TypeOrmModule,EventImagesService,]
+
+
+
+
 })
 export class EventImagesModule {}
