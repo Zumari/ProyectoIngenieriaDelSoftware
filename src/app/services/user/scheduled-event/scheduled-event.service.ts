65b90111d940d6ab2ac3bd5648f2b35fb86c207f
eddyone7 <eddy.giron@unah.hn>
2021-11-26 20:09:50 -0600

    Servicio para evento programado

---
+++ src/app/services/user/scheduled-event/scheduled-event.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ScheduledEvent } from 'src/app/interfaces/scheduled-event';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduledEventService {
+
+  constructor(private http:HttpClient) { }
+
+
+  getAllScheduledEvents():Observable<any[]>{
+    return this.http.get<any[]>(`http://localhost:3000/scheduled-event/getAll`,{});
+  }
+
+  getOneScheduledEvent(scheduledEventId: number):Observable<any>{
+    return this.http.get<any>(`http://localhost:3000/scheduled-event/getScheduledEvent/${scheduledEventId}`,);
+  }
+
+  createScheduledEvent(schEvent:ScheduledEvent):Observable<ScheduledEvent>{
+    return this.http.post<ScheduledEvent>(`http://localhost:3000/scheduled-event/createScheduledEvent`,schEvent);
+  }
+
+  deleteScheduledEvent(scheduledEventId:number):Observable<ScheduledEvent>{
+    return this.http.delete<ScheduledEvent>(`http://localhost:3000/scheduled-event/deleteScheduledEvent/${scheduledEventId}`,{}); 
+  }
+
+  updateScheduledEvent(scheduledEventId:number, schEvent:ScheduledEvent):Observable<ScheduledEvent>{
+    return this.http.put<ScheduledEvent>(`http://localhost:3000/scheduled-event/updateScheduledEvent/:${scheduledEventId}`,schEvent);
+   }
+}
