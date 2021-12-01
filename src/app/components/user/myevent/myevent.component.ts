f467548609e55c6f2bde6c08b66b155834a9f8ae
Fernanda Zúniga <48185765+Zumari@users.noreply.github.com>
2021-11-30 21:38:45 -0600

    correccion pantalla model crear conferencia

--- src/app/components/user/myevent/myevent.component.ts
+++ src/app/components/user/myevent/myevent.component.ts
@@ -7,6 +7,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { InstitutionService } from 'src/app/services/institutions/institutions.service';
 import { ScheduledEventService } from 'src/app/services/user/scheduled-event/scheduled-event.service';
 import { ScheduledEvent } from 'src/app/interfaces/scheduled-event';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { dateValidator, ValidadoresEspeciales } from 'src/app/util/ValidadorEspecial';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-myevent',
@@ -19,6 +22,22 @@ export class MyeventComponent implements OnInit {
   faEye = faEye;
   faAsterisk = faCertificate;
   faTrash = faTrash;
+  public fechaMinima: Date;
+  public fechaStrMinima:string ;
+  mode: string = 'virtual';
+  typeEvent: string = 'conferencia';
+  privacy: string = 'publico';
+
+  conferenciaForm = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.maxLength(50), Validators.pattern('[a-zA-ZÑÁÉÍÓÚáéíóú][a-zA-Zñáéíóú ]{1,}')]),
+    description_:new FormControl('',Validators.compose([Validators.required, Validators.maxLength(300), Validators.pattern('[a-zA-ZÑÁÉÍÓÚáéíóú][a-zA-Zñáéíóú ]{1,}')])),
+    startDate: new FormControl('', [Validators.required, ValidadoresEspeciales.ValidarFechas]),
+    endDate: new FormControl('', [Validators.required]),
+    startTime: new FormControl('', [Validators.required]),
+    endTime: new FormControl('', [Validators.required]),
+    openEvent:  new FormControl(true, [Validators.required]),
+    modality: new FormControl('', Validators.required)
+  },{validators:dateValidator})
 
   event: any = {
     name: '',
@@ -34,7 +53,7 @@ export class MyeventComponent implements OnInit {
     endHour: '',
     image: ''
   };
- 
+
 
   eventosProgramados: ScheduledEvent[]=[{
     scheduledEventId: 0,
@@ -53,17 +72,22 @@ export class MyeventComponent implements OnInit {
     address: ''
    }
   ]
-  
+
 
   inst: any ={
     institutionId:0,
     name:''
   }
-  constructor(private eventServ: EventsService, 
+  constructor(private eventServ: EventsService,
      private activatedRoute:ActivatedRoute,
      private institutionService: InstitutionService,
      private schEvent : ScheduledEventService,
-     private router: Router ) { }
+    private pipe:DatePipe,
+     private router: Router ) {
+
+      this.fechaMinima= new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate());
+      this.fechaStrMinima= this.pipe.transform(this.fechaMinima, "yyyy-MM-dd")!;
+     }
 
   ngOnInit(): void {
     let params= this.activatedRoute.snapshot.params;
@@ -79,10 +103,10 @@ export class MyeventComponent implements OnInit {
 
       },
       error=> console.log(error)
-      
+
     )
-    this.router.navigate(['usuario/mis-eventos']); 
-  }  
+    this.router.navigate(['usuario/mis-eventos']);
+  }
 
   getEvent(id:number){
 
@@ -97,7 +121,7 @@ export class MyeventComponent implements OnInit {
       error => console.log(error)
     )
   }
-  
+
   getAllScheduledEvents(){
     this.schEvent.getAllScheduledEvents().subscribe(
       res =>  {this.eventosProgramados=res},
@@ -110,11 +134,47 @@ export class MyeventComponent implements OnInit {
       res=>{
         console.log(res);
         this.getAllScheduledEvents();
-        
+
       },
       error=> console.log(error)
-      
+
     )
-    
+
+  }
+
+  get name(){
+    return this.conferenciaForm.get('name');
+  }
+  get description_(){
+    return this.conferenciaForm.get('description_');
+  }
+  get startDate(){
+    return this.conferenciaForm.get('startDate');
+  }
+  get endDate(){
+    return this.conferenciaForm.get('endDate');
+  }
+  get places(){
+    return this.conferenciaForm.get('places');
+  }
+  get openEvent(){
+    return this.conferenciaForm.get('openEvent');
+  }
+  get institutionId(){
+    return this.conferenciaForm.get('institutionId');
+  }
+  get modality(){
+    return this.conferenciaForm.get('modality');
+  }
+
+  createEvents() {
+  }
+
+  changePriv(val: number) {
+    this.privacy = val == 1 ? 'publico' : 'privado';
+  }
+
+  changeType(val: number) {
+    this.typeEvent = val == 1 ? 'conferencia': 'taller';
   }
 }
